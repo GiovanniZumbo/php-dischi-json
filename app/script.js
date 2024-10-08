bf115ff6967be3f62ed14e3db7b4eb6d835a33f1
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-08 15:42:36 +0200

    added axios

--- app/script.js
+++ app/script.js
@@ -1,9 +1,28 @@
-const { createApp } = Vue
+const { createApp } = Vue;
+
 
 createApp({
   data() {
     return {
+albumList: [],
+apiUrl: "",
+    }
+  },
 
+  methods:{
+    getAlbumList(){
+        axios.get('/user?ID=12345')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    },
+    created(){
+        this.getAlbumList();
     }
   }
 }).mount('#app')
